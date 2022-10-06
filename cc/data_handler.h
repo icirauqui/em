75d461332229106a96bfffc8398eb3a879ca02ce
icirauqui <icirauqui@gmail.com>
2022-10-06 06:28:01 +0200

    sync

---
+++ cc/data_handler.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+
+void load_data(std::string& data_dir,
+                        std::vector<std::vector<float>> &sets_x, 
+                        std::vector<std::vector<float>> &sets_y,
+                        int limit = 0);
+
+
+
+
+
+
+
+
+
+
+
+
